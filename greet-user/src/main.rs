d9a32c96e0738600289ef73b961696774ee2b30d
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-02-17 20:56:44 +0900

    4章終了

--- greet-user/src/main.rs
+++ greet-user/src/main.rs
@@ -1,5 +1,13 @@
 use clap::Parser;
-use wasmtime::{component::{Component, Linker, TypedFunc},Engine, Store};
+use wasmtime::{Engine, Store};
+use wasmtime::component::{bindgen, Component, Linker, TypedFunc};
+
+// ファイルの先頭でマクロを実行
+// {} の間にコード生成のための設定を記述
+bindgen!({
+    world: "greetable-provider",  // コード生成を行うワールドの名前
+    path: "../greet/wit",  // WITファイルが存在するフォルダーへのパス
+});
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -15,6 +23,7 @@ fn start(args: Args) -> anyhow::Result<()> {
     // Storeオブジェクトを作成      // 第二引数はStoreオブジェクトの初期値
     // Componentオブジェクトを作成するごとにStoreオブジェクトの状態が変化するのでmut
     let mut store = Store::new(&engine, ());
+    
     // Componentオブジェクトからインスタンスを作成
     let instance = linker.instantiate(&mut store, &component)?;
 
@@ -55,10 +64,43 @@ fn start(args: Args) -> anyhow::Result<()> {
     Ok(())
 }
 
+fn start45(args: Args) -> anyhow::Result<()> {
+    let engine = Engine::default();
+    let component = Component::from_file(&engine, &args.wasm_file)?;
+    let linker = Linker::new(&engine);
+    let mut store = Store::new(&engine, ());
+
+    // 以下からが章の変更点
+
+    // ロードしたライブラリコンポーネントのインスタンスを作成
+    let provider = GreetableProvider::instantiate(&mut store, &component, &linker)?;
+
+    // greetableインターフェースの実装をラップしたオブジェクト
+    let greetable = provider.your_namespace_greet_greetable();
+
+    // greet関数を呼び出す &strを渡す（前章との変更点）
+    let message = greetable.call_greet(&mut store, "world")?;
+    println!("{message}");
+
+    // name関数を呼び出す
+    let name = greetable.call_name(&mut store)?;
+    let message = greetable.call_greet(&mut store, &name)?;
+    println!("{message}");
+
+    Ok(())
+}
+
+
 fn main() {
     let args = Args::parse();
 
-    if let Err(e) = start(args) {
-        println!("{:?}", e);
+    // ~4.4
+    // if let Err(e) = start(args) {
+    //     println!("{:?}", e);
+    // }
+
+    // 4.5
+    if let Err(e) = start45(args) {
+        println!("{:?}",e);
     }
 }
